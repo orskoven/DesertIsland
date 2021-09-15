b99086a5c4ffa3cbf1ce32ebffb7f1e46153d29a
Simon Ørskov Dieckmann Pedersen <simonorskov@gmail.com>
2021-09-15 17:30:19 +0200

    completed the project

--- Src/Main.java
+++ Src/Main.java
@@ -1,55 +1,122 @@
+import java.util.Arrays;
+import java.util.OptionalInt;
 import java.util.Scanner;
 
 
 
 public class Main {
 
-    public static String optionsInput() {
-        int[] scoreKeeper = new int[10];
-            Scanner scanner = new Scanner(System.in);
+    public static int optionsInput(int arrayInput) {
+            int[] Arrays = new int[10];
             String[] options =
-                    {"kæreste", "kaffemaskine", "Netflix", "sofa", "løbesko",
+                    {"skattermaus", "kaffemaskine", "Netflix", "sofa", "løbesko",
                             "guitar", "slik", "hund", "bog", "øl"};
-            int arrayInput = scanner.nextInt();
-            String inputToArray = (arrayInput == 1) ? options[0] : (arrayInput == 2) ? options[1] : (arrayInput == 3) ? options[2] :
-                    (arrayInput == 4) ? options[3] : (arrayInput == 5) ? options[4] : (arrayInput == 6) ? options[5] :
-                            (arrayInput == 7) ? options[6] : (arrayInput == 8) ? options[7] : (arrayInput == 9) ? options[8] :
-                                    (arrayInput == 10) ? options[9] : "Try again!";
-            int scoreKeeping = (arrayInput == 1)?scoreKeeper[0] : (arrayInput == 2) ? scoreKeeper[1] : (arrayInput == 3) ? scoreKeeper[2] :
-                    (arrayInput == 4) ? scoreKeeper[3] : (arrayInput == 5) ? scoreKeeper[4] : (arrayInput == 6) ? scoreKeeper[5] :
-                            (arrayInput == 7) ? scoreKeeper[6] : (arrayInput == 8) ? scoreKeeper[7] : (arrayInput == 9) ? scoreKeeper[8] : scoreKeeper[9];
-            System.out.println(inputToArray);
-
-            return inputToArray;
+            int inputToArray = (arrayInput == 1) ? 0 : (arrayInput == 2) ? 1 : (arrayInput == 3) ? 2 :
+                    (arrayInput == 4) ? 3 : (arrayInput == 5) ? 4 : (arrayInput == 6) ? 5 :
+                            (arrayInput == 7) ? 6 : (arrayInput == 8) ? 7 : (arrayInput == 9) ? 8 : (arrayInput == 10) ? 9 : 11;
+           System.out.println(options[inputToArray]);
+        return inputToArray;
     }
+
+public static int scoreConverter(int score){
+            int inputToArray = (score == 1) ? 0 : (score == 2) ? 1 : (score == 3) ? 2 :
+                    (score == 4) ? 3 : (score == 5) ? 4 : (score == 6) ? 5 :
+                            (score == 7) ? 6 : (score == 8) ? 7 : (score == 9) ? 8 : 9;
+
+        return inputToArray;
+}
+public static OptionalInt largestNumber(int[] arrayInput){
+    OptionalInt theLargestNumber = Arrays.stream(arrayInput).max();
+    return theLargestNumber;
+
+}
+
 
 
 
 
     public static void main(String[] args) {
-        /*
-        String[] options =
-                {"kæreste","kaffemaskine","Netflix","sofa","løbesko",
-                        "guitar","slik", "hund", "bog","øl"};
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Hvilke ting vil du helst have med på en øde ø?"
-                +"\n1 kæreste\n2 kaffemaskine\n3 Netflix\n4 sofa\n5 løbesko\n6 guitar\n7 slik\n8 hund\n9 bog\n10 øl\nIndtast valg:" );
-        int[]  arrayInput = new int[10];
-        String inputToArray = (scanner.nextInt() == 1)?options[0]:(scanner.nextInt() == 2)?options[1]:(scanner.nextInt() == 3)?options[2]:
-                (scanner.nextInt() == 4)?options[3]:(scanner.nextInt() == 5)?options[4]:(scanner.nextInt() == 6)?options[5]:
-                        (scanner.nextInt() == 7)?options[6]:(scanner.nextInt() == 8)?options[7]:(scanner.nextInt() == 9)?options[8]:
-                                (scanner.nextInt() == 10)?options[9]:"Try again!";
-        System.out.println(options);
-
-         */
-        System.out.println("Hvilke ting vil du helst have med på en øde ø?"
-                + "\n1 kæreste\n2 kaffemaskine\n3 Netflix\n4 sofa\n5 løbesko\n6 guitar\n7 slik\n8 hund\n9 bog\n10 øl\n");
+                + "\n1 skattermaus\n2 kaffemaskine\n3 Netflix\n4 sofa\n5 løbesko\n6 guitar\n7 slik\n8 hund\n9 bog\n10 øl\n");
+        int[] arrays = new int[10];
         int i = 0;
-        do { System.out.println("Indtast værdi: ");
-        i++;
-        optionsInput();
-    } while( i < 10);
+        do {
+            String[] options =
+                    {"skattermaus", "kaffemaskine", "Netflix", "sofa", "løbesko",
+                            "guitar", "slik", "hund", "bog", "øl"};
+            System.out.println("Indtast værdi: ");
+            Scanner scanner = new Scanner(System.in);
+            int arrayInput = scanner.nextInt();
+            optionsInput(arrayInput);
+            arrays[scoreConverter(arrayInput)]++;
+            Arrays.stream(arrays).count();
 
 
+            if (i == 9) {
+                System.out.println(Arrays.toString(arrays));
+                if (arrays[4] > arrays[1] && arrays[4] > arrays[2] && arrays[4] > arrays[0]
+                        && arrays[4] > arrays[3] && arrays[4] > arrays[5]
+                        && arrays[4] > arrays[6] && arrays[4] > arrays[7]
+                        && arrays[4] > arrays[8] && arrays[4] > arrays[9]) {
+                    System.out.println("Det mest valgte er " + options[4] + ".");
+                }
+                else if (arrays[1] > arrays[2] && arrays[1] > arrays[0]
+                        && arrays[1] > arrays[3] && arrays[1] > arrays[5]
+                        && arrays[1] > arrays[6] && arrays[1] > arrays[7]
+                        && arrays[1] > arrays[8] && arrays[1] > arrays[9]) {
+                    System.out.println("Det mest valgte er " + options[1] + ".");
+                }
+                else if (arrays[2] > arrays[1] &&
+                        arrays[2] > arrays[3] && arrays[2] > arrays[5] && arrays[2] > arrays[0]
+                        && arrays[2] > arrays[6] && arrays[2] > arrays[7]
+                        && arrays[2] > arrays[8] && arrays[2] > arrays[9]) {
+                    System.out.println("Det mest valgte er " + options[2] + ".");
+                }
+                else if(arrays[3] > arrays[1] && arrays[3] > arrays[5] && arrays[3] > arrays[0]
+                        && arrays[3] > arrays[6] && arrays[3] > arrays[7]
+                        && arrays[3] > arrays[8] && arrays[3] > arrays[9]) {
+                    System.out.println("Det mest valgte er " + options[3] + ".");
+                }
+                else if (arrays[5] > arrays[1] && arrays[5] > arrays[3] && arrays[5] > arrays[0]
+                        && arrays[5] > arrays[6] && arrays[5] > arrays[7]
+                        && arrays[5] > arrays[8] && arrays[5] > arrays[9]) {
+                    System.out.println("Det mest valgte er " + options[6] + ".");
+                }
+                else if (arrays[7] > arrays[1] && arrays[7] > arrays[2] && arrays[7] > arrays[0]
+                        && arrays[7] > arrays[3] && arrays[7] > arrays[5]
+                        && arrays[7] > arrays[6] && arrays[7] > arrays[8]
+                        && arrays[7] > arrays[9]) {
+                    System.out.println("Det mest valgte er " + options[7] + ".");
+                }
+                else if (arrays[8] > arrays[2] && arrays[8] > arrays[0]
+                        && arrays[8] > arrays[3] && arrays[8] > arrays[5]
+                        && arrays[8] > arrays[6] && arrays[8] > arrays[7] &&
+                        arrays[8] > arrays[9] && arrays[8] > arrays[1]) {
+                    System.out.println("Det mest valgte er " + options[8] + ".");
+                }
+                else if (arrays[0] > arrays[2] && arrays[0] > arrays[1]
+                        && arrays[0] > arrays[3] && arrays[0] > arrays[5]
+                        && arrays[0] > arrays[6] && arrays[0] > arrays[7]
+                        && arrays[0] > arrays[8] && arrays[0] > arrays[9]) {
+                    System.out.println("Det mest valgte er " + options[0] + ".");
+
+                } else if (arrays[6] > arrays[1] && arrays[6] > arrays[3] && arrays[6] > arrays[0]
+                        && arrays[6] > arrays[7]
+                        && arrays[6] > arrays[8] && arrays[6] > arrays[9]) {
+                    System.out.println("Det mest valgte er " + options[6] + ".");
+
+                } else if (arrays[9] > arrays[1] && arrays[9] > arrays[3] && arrays[9] > arrays[0]
+                        && arrays[9] > arrays[6] && arrays[9] > arrays[7]
+                        && arrays[9] > arrays[8]) {
+                    System.out.println("Det mest valgte er " + options[9] + ".");
+
+                } else {
+                    System.out.println("ingen favoritter :(");
+                }
+
+            }i++;
+        }while (i < 10);
+
+
+        }
     }
-}
